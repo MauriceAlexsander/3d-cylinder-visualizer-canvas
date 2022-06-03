e3944bf8fe9d7414b225eaae849cef66644f0bca
lMizu <mauricezanotelli@gmail.com>
2022-06-03 12:29:05 -0300

    Form: create component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,31 +1,70 @@
 <template>
-  <div class="form">
-    <input type="text" class="form-control">
-    <input type="text" class="form-control">
-    {{props.msg}}
-    <button type="submit" class="btn">Calcular</button>
+  <div class="form-container">
+    <form>
+      <label for="height">height (meters)</label>
+      <input v-model="height" name="height" type="number" class="form-control" min="0" step="any">
+
+      <label for="radius">radius (meters)</label>
+      <input v-model="radius" name="radius" type="number" class="form-control" min="0" step="any">
+
+      <p>Volume: {{volume}}</p>
+    </form>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
+import { defineProps, ComputedRef, computed, defineEmits, WritableComputedRef } from "vue";
+const emit = defineEmits(['update:height_model', 'update:radius_model'])
 
-const props = defineProps({msg: String})
+// Props
+const props = defineProps(['height_model', 'radius_model'])
+
+// Computed
+const height: WritableComputedRef<number> = computed({
+  get() {
+    return props.height_model
+  },
+  set(value) {
+    emit('update:height_model', value)
+  }
+})
+const radius: WritableComputedRef<number> = computed({
+  get() {
+    return props.radius_model
+  },
+  set(value) {
+    emit('update:radius_model', value)
+  }
+})
+const volume: ComputedRef<string> = computed(() => `${(Math.PI * Math.pow(radius.value, 2) * height.value).toFixed(2)} m²`)
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.form-container {
+  display: flex;
+  justify-content: center;
+  color: #F2CC8F;
+  width: 50%;
+  font-size: 2rem;
+
+  form {
+    width: clamp(320px, 90%, 720px);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    box-sizing: border-box;
+    padding: 0 48px;
+  }
+
+  input {
+    height: 48px;
+    margin: 6px 0;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+@media screen and (max-width: 1080px) {
+  .form-container {
+    width: 100%;
+  }
 }
 </style>
