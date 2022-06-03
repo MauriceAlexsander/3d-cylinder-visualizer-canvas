ffa3e6354deb937ca3c0acbefac3cbb32b6ab86e
lMizu <mauricezanotelli@gmail.com>
2022-06-03 01:02:55 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cilinder
+# cylinder 
 
 ## Project setup
 ```
---
+++ src/components/Form.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form">
+    <input type="text" class="form-control">
+    <input type="text" class="form-control">
+    {{props.msg}}
+    <button type="submit" class="btn">Calcular</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps({msg: String})
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
